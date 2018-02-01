a79d9bdf4ae0dd09ff97c6490faa425bf9a29165
lyakaap <lyakaap@has-key.org>
2018-02-01 22:49:01 +0900

    Create vat.py

---
+++ vat.py
@@ -0,0 +1,51 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+def _l2_normalize(d):
+    if isinstance(d, Variable):
+        d = d.data.numpy()
+    elif isinstance(d, torch.Tensor):
+        d = d.numpy()
+    d /= (np.sqrt(np.sum(d ** 2, axis=(1, 2, 3))).reshape((-1, 1, 1, 1)) + 1e-16)
+    return torch.from_numpy(d)
+
+
+def vat_loss(model, X, xi=0.1, eps=1.0, Ip=1, use_gpu=True):
+    """
+    :param model: networks to train
+    :param X: Variable, input
+    :param xi: hyperparameter of VAT (default: 1.0)
+    :param eps: hyperparameter of VAT (default: 1.0)
+    :param Ip: iteration times of computing adv noise (default: 1)
+    :param use_gpu: use gpu or not
+    :return: LDS, classification loss, model prediction
+    """
+    kl_div = nn.KLDivLoss()
+    if use_gpu:
+        kl_div.cuda()
+
+    pred = model(X)
+
+    # calc adversarial direction
+    d = torch.rand(X.shape)
+    d = Variable(_l2_normalize(d))
+    if use_gpu:
+        d = d.cuda()
+    
+    for ip in range(Ip):
+        d.requires_grad = True
+        pred_hat = model(X + d / xi)
+        adv_distance = kl_div(F.log_softmax(pred_hat, dim=1), pred.detach())
+        adv_distance.backward()
+        d = Variable(_l2_normalize(d.grad.data))
+        model.zero_grad()
+
+    r_adv = d * eps
+    pred_hat = model(X + r_adv)
+    pred = model(X)
+    LDS = kl_div(F.log_softmax(pred_hat, dim=1), pred.detach())
+    return LDS, pred
